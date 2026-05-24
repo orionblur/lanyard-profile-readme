0291eb4aa1f7b2884827f72e8acf7ba690405ace
orionblur <252493356+orionblur@users.noreply.github.com>
2026-05-23 23:03:30 -0700

    Wrong URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const ORIGIN_URL =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://lanyard-profile.orionblur.com";
+      : "https://lanyard.orionblur.com";
 
   const [userId, setUserId] = useState("");
   const [userError, setUserError] = useState<string | JSX.Element>();
